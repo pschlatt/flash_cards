c89adc39306dccaad2f636eb6db8321277b69e5c
Paul Schlattmann <paul.h.schlattmann@gmail.com>
2019-01-27 22:33:20 -0700

    Updated functionality and Tests

--- lib/round.rb
+++ lib/round.rb
@@ -1,5 +1,7 @@
 require './lib/card'
 require './lib/deck'
+require './lib/turn'
+
 
 
 class Round
@@ -10,22 +12,39 @@ attr_reader :deck, :turns
     @turns = []
   end
 
-  def turns
-    @turns
+  def take_turn(guess)
+       new_turns = []
+       new_turns << Turn.new(guess, current_card)
+       @turns << new_turns
+       @deck.cards.rotate!
+      return  new_turns
   end
+
 
   def current_card
-    counter = 0
-    p deck[counter]
-    counter += 1
-
+    deck.cards.first
   end
 
-  def take_turn(guess)
-    counter = 0
-    current_turn = Turn.new(guess, deck[counter])
-    @turns << current_turn if deck[counter] == current_turn
-    counter += 1
+  def number_correct
+   turns.count {|turn| turn.correct?}
   end
+
+  def number_correct_by_category(category)
+    turns.count do |turn|
+      turn.correct? && turn.card.category == category
+    end
+  end
+
+  def percent_correct
+    (number_correct.to_f / turns.length) * 100
+  end
+
+  def percent_correct_by_category(category)
+   turns_in_category = turns.count do |turn|
+     turn.card.category == category
+   end
+   percent = (number_correct_by_category(category).to_f / (turns_in_category)) * 100
+  end
+
 
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
 require './lib/deck'
+require './lib/turn'
 require './lib/round'
 
 class RoundTest < Minitest::Test
@@ -15,6 +16,7 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
 
     assert_instance_of Round, round
+    assert_instance_of Deck, round.deck
   end
 
   def test_turn_exists_and_is_nil
@@ -38,20 +40,25 @@ def test_current_card
   round = Round.new(deck)
 
 
-  assert_equal deck[0], round.current_card
+  assert_equal card_1, round.current_card
 
 end
 
   def test_takes_turn
-    skip
+
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
+    new_turn = Turn.new("Juneau", card_1)
 
+    take_a_turn = round.take_turn(card_1.answer)
 
-  end
+    assert_instance_of Turn, new_turn
+    assert_equal [new_turn], take_a_turn
+
+   end
 
   end
