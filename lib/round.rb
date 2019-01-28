a6f0ac610f156f19efcf970acf07d8dc170dd07a
Paul Schlattmann <paul.h.schlattmann@gmail.com>
2019-01-28 08:29:18 -0700

    Added Start Method

--- lib/round.rb
+++ lib/round.rb
@@ -45,4 +45,35 @@ attr_reader :deck, :turns
   end
 
 
+  def start
+
+    puts ""
+    puts ""
+    puts "Welcome! You're playing with #{deck.cards.count} cards"
+    puts "-" * 79
+    # iterate for each question
+    questions = deck.cards.map do |card|
+      "This is card number #{deck.cards.index(card)} of #{deck.cards.count}."
+      "Question: #{card.question}"
+    end
+    questions.each do |question|
+      puts "This is card number #{turns.count + 1} out of #{deck.cards.count}"
+      puts question
+      input = gets.chomp.downcase.capitalize
+      take_turn(input)
+      puts turns.last.feedback
+
+    end
+
+    puts "****** Game over! ******"
+    puts "You had #{number_correct} correct guesses out of #{deck.cards.count} for a total score of #{percent_correct.to_i}%"
+
+
+      cards.category.each do |card|
+      puts "#{card.category.to_s} - #{percent_correct_by_category(card.category).to_i} % correct"
+    end
+    puts ""
+    puts ""
+  end
+
 end
